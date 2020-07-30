b94cadf0598cb72b779fe1cf65bf46990d4908dd
tesla7 <77625961@qq.com>
2020-07-30 17:09:41 +0800

    update conf.py

--- fml_manager/source/conf.py
+++ fml_manager/source/conf.py
@@ -49,7 +49,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
