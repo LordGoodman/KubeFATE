0ab4a1f8635be88b64ef869073b0653db27378a7
haozheng <77625961@qq.com>
2020-07-30 15:02:11 +0800

    Update __init__.py

--- fml_manager/fml_manager/__init__.py
+++ fml_manager/fml_manager/__init__.py
@@ -10,6 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from fml_manager.fml_manager import FMLManager
-from fml_manager.fml_cluster_manager import ClusterManager
-from fml_manager.utils.fate_builders import *
+# from fml_manager.fml_manager import FMLManager
+# from fml_manager.fml_cluster_manager import ClusterManager
+# from fml_manager.utils.fate_builders import *
+from .utils import *
+from . import *
